14786340e6f6338ca6ffae882ef3fc6dfe58b335
Wintermute0110 <wintermute0110@gmail.com>
2018-11-08 22:51:32 +0900

    Fix script.

--- install-build-dependencies-debian.sh
+++ install-build-dependencies-debian.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-# Install PCSX2 build dependencies for Debian unstable (October 2018).
-# See https://wiki.debian.org/Multiarch/HOWTO
-# NOTE a chroot is required! Otherwise the dependencies will mess with the 64 bit system.
-
-# Enable i386 architecture
-# After enabling a new architecture execute `apt-get update`
-dpkg --add-architecture i386
+# Install PCSX2 build dependencies for Debian unstable (October 2018) in a i386 chroot.
+#
+# NOTE that a chroot is required! Otherwise the dependencies will mess with the 64 bit system.
+#
 
 # Install build tools
-apt install build-essential cmake gcc-multilib g++-multilib
+apt install build-essential cmake
 
 # Install build libraries
 apt install \
-    libasound2-dev:i386 \
-    liblzma-dev:i386 \
-    zlib1g-dev:i386 \
-    libpng-dev:i386 \
-    libbz2-dev:i386 \
-    libjpeg-dev:i386
+    libasound2-dev \
+    liblzma-dev \
+    zlib1g-dev \
+    libpng-dev \
+    libbz2-dev \
+    libjpeg-dev
 
 # Install complex build libraries
 # CG -> nvidia-cg-toolkit
 #
 apt install \
-    libsdl2-dev:i386 \
-    libwxgtk3.0-dev:i386 \
-    libgl1-mesa-dev:i386 \
-    libglew-dev:i386 \
-    nvidia-cg-toolkit:i386 \
-    libgtk2.0-dev:i386
+    libsdl2-dev \
+    libwxgtk3.0-dev \
+    libgl1-mesa-dev \
+    libglew-dev \
+    nvidia-cg-toolkit \
+    libgtk2.0-dev
